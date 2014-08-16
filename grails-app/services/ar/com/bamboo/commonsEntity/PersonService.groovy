494ea554ca767273fcf7ce4afb8278473c292620
orko <orquito@gmail.com>
2014-08-16 14:41:05 -0300

    Agrego métodos de búsqueda completa a person service

--- grails-app/services/ar/com/bamboo/commonsEntity/PersonService.groovy
+++ grails-app/services/ar/com/bamboo/commonsEntity/PersonService.groovy
@@ -7,9 +7,21 @@ import grails.transaction.Transactional
 class PersonService extends BaseService{
 
     @Transactional(readOnly = true)
-    List<Object> list(Map params) {
+    public List<Object> list(Map params) {
         def where = { enabled == true } as DetachedCriteria<Person>
         return this.listWithLimit(Person.class, where, params)
     }
 
+
+    @Transactional(readOnly = true)
+    public List<Person> listAll() {
+        def where = { enabled == true } as DetachedCriteria<Person>
+        return this.listAll(Person.class, where)
+    }
+
+    @Transactional(readOnly = true)
+    public List<Person> listWithLimit(params) {
+        def where = { enabled == true } as DetachedCriteria<Person>
+        return this.listWithLimit(Person.class, where, params)
+    }
 }
