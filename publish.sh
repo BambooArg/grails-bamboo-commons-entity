d255a884a80ae373dc489867beada6c7dc828ead
orko <orquito@gmail.com>
2014-09-14 10:24:16 -0300

    Cambio en la forma de publicar para que publique en el maven local

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "instalando bamboo commons-entity"
-grails clean && grails refresh-dependencies && grails maven-install
+grails clean && grails refresh-dependencies && grails publish-plugin
 
 #grails publish-plugin 
 
