67c89b6e5f49235cf920096ff2c9b7ae3c7decb0
Orko <orquito@gmail.com>
2015-03-09 07:27:27 -0300

    Agrego método para manejar si una provincia es la que dice ser.

--- grails-app/domain/ar/com/bamboo/commonsEntity/Department.groovy
+++ grails-app/domain/ar/com/bamboo/commonsEntity/Department.groovy
@@ -3,6 +3,7 @@ package ar.com.bamboo.commonsEntity
 import ar.com.bamboo.framework.domains.BaseEntity
 
 class Department extends BaseEntity{
+    private static final Integer ID_CAPITAL_FEDERAL = 1
 
     String name
     Province province
@@ -14,5 +15,9 @@ class Department extends BaseEntity{
     @Override
     protected void executeMoreBeforeInsert() {
 
+    }
+
+    boolean isCapitalFederal(){
+        return this.id == ID_CAPITAL_FEDERAL
     }
 }
