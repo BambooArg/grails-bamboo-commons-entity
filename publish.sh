a98f7410361015ff58b1538bdd24cce1d694f1ab
Orko <orquito@gmail.com>
2015-03-19 07:44:52 -0300

    Modifico el script de publis.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "instalando bamboo commons-entity"
-grails clean && grails refresh-dependencies && grails publish-plugin
+grails clean && grails refresh-dependencies && grails maven-install
 
 #grails publish-plugin 
 
